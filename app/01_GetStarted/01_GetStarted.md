7bbcf93be7508186a9291852c4aa709ce01cfb41
Tsukumo3 <tkmkik@outlook.jp>
2021-12-19 02:36:59 +0900

    add 01_GetStarted

---
+++ app/01_GetStarted/01_GetStarted.md
@@ -0,0 +1,22 @@
+# 01 GetStarted - CountDownTimer
+
+```
+CountDownTimer[TimerName=B,Counter=0]
+CountDownTimer[TimerName=B,Counter=1]
+CountDownTimer[TimerName=C,Counter=0]
+CountDownTimer[TimerName=A,Counter=0]
+CountDownTimer[TimerName=A,Counter=1]
+CountDownTimer[TimerName=C,Counter=1]
+CountDownTimer[TimerName=B,Counter=2]
+CountDownTimer[TimerName=A,Counter=2]
+CountDownTimer[TimerName=C,Counter=2]
+CountDownTimer[TimerName=B,Counter=3]
+CountDownTimer[TimerName=C,Counter=3]
+CountDownTimer[TimerName=C,Counter=4]
+CountDownTimer[TimerName=A,Counter=3]
+CountDownTimer[TimerName=B,Counter=4]
+CountDownTimer[TimerName=B,Counter=5] === Finish: B ===
+CountDownTimer[TimerName=C,Counter=5] === Finish: C ===
+CountDownTimer[TimerName=A,Counter=4]
+CountDownTimer[TimerName=A,Counter=5] === Finish: A ===
+```
---
+++ app/01_GetStarted/CountDownTimer.java
@@ -0,0 +1,69 @@
+class CountDownTimer extends Thread{
+
+    final int MAX_COUNT = 5;
+    final long millis = 10L;
+    private int Counter = 0;
+    private String TimerName = "";
+
+    CountDownTimer(String aName)
+    {
+        this.TimerName = aName;
+        return;
+    }
+
+    private String GetTimerName()
+    {
+        return TimerName;
+    }
+
+    @Override
+    public void run() {
+        while (!isMaximumCount()) {
+            tick();      
+        }
+        beep();
+        return;
+    }
+
+    private void tick()
+    {
+        System.out.println(this);
+        Counter += 1;
+        return;
+    }
+
+    private void beep()
+    {
+        System.out.println(this +" === Finish: " + GetTimerName() + " ===");
+        return;
+    }
+
+    private Boolean isMaximumCount()
+    {
+        return Counter == MAX_COUNT ? true : false;
+    }
+
+    @Override
+	public String toString()
+	{
+		StringBuffer aBuffer = new StringBuffer();
+		Class<?> aClass = this.getClass();
+		aBuffer.append(aClass.getName());
+		aBuffer.append("[TimerName=");
+		aBuffer.append(TimerName);
+		aBuffer.append(",Counter=");
+		aBuffer.append(Counter);
+		aBuffer.append("]");
+		return aBuffer.toString();
+	}
+
+    public static void main(String[] args)
+    {
+        CountDownTimer timerA = new CountDownTimer("A");
+        CountDownTimer timerB = new CountDownTimer("B");
+        CountDownTimer timerC = new CountDownTimer("C");
+        timerA.start();
+        timerB.start();
+        timerC.start();
+    }
+}
